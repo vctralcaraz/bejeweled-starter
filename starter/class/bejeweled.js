257d709ce9121d47dafbf98976ca1f68ab4fde4b
Victor Alcaraz <vctralcaraz@gmail.com>
2023-09-14 22:42:45 -0700

    first big push: randomized grid to fruits and am able to select fruits changing the color to cyan

--- starter/class/bejeweled.js
+++ starter/class/bejeweled.js
@@ -5,17 +5,51 @@ class Bejeweled {
 
   constructor() {
 
-    this.playerTurn = "O";
+    // this.playerTurn = "O";
+    this.numRows = 8;
+    this.numCols = 8;
+    this.icons = ['🍇', '🍉', '🍊', '🍋', '🍍', '🍒', '🍓', '🥝', '🥥'];
 
     // Initialize this
-    this.grid = [];
+    this.grid = [
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+    ];
+
+    this.initializeBoard();
 
     this.cursor = new Cursor(8, 8);
 
     Screen.initialize(8, 8);
+    Screen.grid = this.grid;
     Screen.setGridlines(false);
 
     this.cursor.setBackgroundColor();
+
+    Screen.addCommand('up', 'Move up', () => {
+      this.cursor.up();
+    });
+    
+    Screen.addCommand('right', 'Move right', () => {
+      this.cursor.right();
+    });
+    Screen.addCommand('down', 'Move down', () => {
+      this.cursor.down();
+    });
+    Screen.addCommand('left', 'Move left', () => {
+      this.cursor.left();
+    });
+    Screen.addCommand('return', 'select fruit', () => {
+      this.cursor.select();
+    });
+
+
     Screen.render();
   }
 
@@ -24,6 +58,30 @@ class Bejeweled {
     // Fill this in
 
   }
+
+  initializeBoard() {
+    let lastR = null;
+
+    for(let i = 0; i < this.numRows; i++) {
+      for(let j = 0; j < this.numCols; j++) {
+        let r = this._getRandomIndex(lastR)
+        lastR = r;
+
+        this.grid[i][j] = this.icons[r]
+      }
+    }
+  }
+
+  _getRandomIndex(lastR) {
+    let index = Math.floor(Math.random() * this.icons.length);
+
+    if(index === lastR) {
+      return this._getRandomIndex(lastR)
+    }
+
+    return index;
+  }
+
 
 }
 
